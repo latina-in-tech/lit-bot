cf431f45c4b1c4db0e144f9d0e30cc71076acf71
Francesco Dimuro <francesco.dimuro@adecco.it>
2024-02-28 22:32:34 +0100

    chore: remove unused comments

--- __main__.py
+++ __main__.py
@@ -36,33 +36,10 @@ if __name__ == '__main__':
     cmds_handler = CommandHandler('cmds', cmds)
     application.add_handler(cmds_handler)
 
-    # create_job_handler = ConversationHandler(
-    #     entry_points=[CommandHandler("create_job", create_job)],
-    #     states={
-    #         CreateJob.CATEGORY_ID: [MessageHandler(filters.TEXT, callback=category_id)], # spostare tutto in un dict
-    #         CreateJob.CONTRACT_TYPE: [MessageHandler(filters.TEXT, contract_type)],
-    #         CreateJob.POSITION: [
-    #             MessageHandler(filters.TEXT, position),
-    #         ],
-    #         CreateJob.DESCRIPTION: [MessageHandler(filters.TEXT, description)],
-    #         CreateJob.LINK: [MessageHandler(filters.TEXT, link)],
-    #         CreateJob.RAL: [MessageHandler(filters.TEXT, ral)]
-    #     },
-    #     fallbacks=[CommandHandler("cancel", lambda _: ConversationHandler.END)],
-    # )
-
     create_job_handler = ConversationHandler(**create_job_handler)
     application.add_handler(create_job_handler)
     
     unknown_handler = MessageHandler(filters.COMMAND, unknown)
     application.add_handler(unknown_handler)
 
-    application.run_polling()
-
-    # /create_event
-    # /retrieve_event
-    # /retrieve_events
-    # /update_event
-    # /delete_event (soft-delete)
-
-    
+    application.run_polling()    
