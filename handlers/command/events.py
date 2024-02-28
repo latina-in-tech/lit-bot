ed7ca4b1eb96090ab8564a20c5248b46107a472e
Francesco Dimuro <francesco.dimuro@adecco.it>
2024-02-28 10:12:07 +0100

    chore: add comments and script enhancement

--- handlers/command/events.py
+++ handlers/command/events.py
@@ -9,27 +9,33 @@ from telegram.constants import ParseMode
 
 async def events(update: Update, context: ContextTypes.DEFAULT_TYPE, db_session: Session = get_db()):
 
+    # Variables initialization
     events_count: int = 0
     text: str = ''
 
+    # Select statement for the events' list
     select_statement: Select = select(Event) \
                                .where(Event.deleted_at.is_(None)) \
                                .order_by(Event.date)
     
+    # Execute the query and get the result
     query_result: ScalarResult = db_session.scalars(select_statement).all()
 
+    # If the query returned some data
     if query_result:
+
+        # Get the records' count
         events_count = len(query_result)
+    
+        # Set the text to display to the user
+        text = f'\U000025b6 Numero totale di eventi: {events_count}\n'
 
-        if events_count:
-            text = f'\U000025b6 Numero totale di eventi: {events_count}\n'
-
-            for i, event in enumerate(query_result):
-                text += f'{i + 1}. <a href="{event.link}">{event.name}</a> - ' \
-                        f'{datetime.strftime(event.date, '%d/%m/%Y %H:%M:%S')}\n'
-                    
-            await context.bot.send_message(chat_id=update.effective_chat.id, text=text, parse_mode=ParseMode.HTML)
+        # Compose the list of events
+        for i, event in enumerate(query_result):
+            text += f'{i + 1}. <a href="{event.link}">{event.name}</a> - ' \
+                    f'{datetime.strftime(event.date, '%d/%m/%Y %H:%M:%S')}\n'
+                
+        # Send the message to the user
+        await context.bot.send_message(chat_id=update.effective_chat.id, text=text, parse_mode=ParseMode.HTML)
     else:
         await context.bot.send_message(chat_id=update.effective_chat.id, text='Nessun evento trovato!')
-
-        return
--- handlers/command/jobs.py
+++ handlers/command/jobs.py
@@ -8,22 +8,30 @@ from telegram.constants import ParseMode
 
 async def jobs(update: Update, context: ContextTypes.DEFAULT_TYPE, db_session: Session = get_db()):
 
+    # Variables initialization
     query_parameters: dict = {}
+    jobs_count: int = 0
+    text: str = ''
 
+    # Check for the presence of arguments passed to the message
     for query_parameter in context.args:
+
+        # Get the parameter name and value
         parameter_name, parameter_value = query_parameter.split('=')
+        
+        # If the parameter is not in the model's columns
         if parameter_name not in Job.__dict__.keys():
             await context.bot.send_message(chat_id=update.effective_chat.id, text=f'Parametro non corretto: {parameter_name}')
             return
         
+        # Else, assign it to the query parameters dictionary
         query_parameters[parameter_name] = parameter_value
     
-    category_id: int | None = int(query_parameters.get('category_id', None))
-    contract_type: int | None = int(query_parameters.get('contract_type_id', None))
+    # Get the query parameters (if they are present)
+    category_id: int | None = int(query_parameters.get('category_id', 0))
+    contract_type: int | None = int(query_parameters.get('contract_type_id', 0))
     
-    jobs_count: int = 0
-    text: str = ''
-
+    # Select statement for the jobs' list
     select_statement: Select = select(Job) \
                                .where(
                                    and_(
@@ -34,20 +42,24 @@ async def jobs(update: Update, context: ContextTypes.DEFAULT_TYPE, db_session: S
                                 ) \
                                .order_by(Job.created_at)
     
+    # Execute the query and get the result
     query_result: ScalarResult = db_session.scalars(select_statement).all()
 
+    # If the query returned some data
     if query_result:
         
+        # Get the records' count
         jobs_count = len(query_result)
 
+        # Set the text to display to the user
         text = f'\U000025b6 Numero totale di lavori: {jobs_count} - Parametri ({query_parameters}):\n'
 
+        # Compose the list of jobs
         for i, job in enumerate(query_result):
             text += f'{i + 1}. <a href="{job.link}">{job.position}</a> - ' \
                     f'{job.job_category.name}\n'
                 
+        # Send the message to the user
         await context.bot.send_message(chat_id=update.effective_chat.id, text=text, parse_mode=ParseMode.HTML)
     else:
         await context.bot.send_message(chat_id=update.effective_chat.id, text='Nessun lavoro trovato!')
-
-        return
