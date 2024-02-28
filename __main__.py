5151f6655dcaad18d0d09ef3bcb50568ffa056f5
Francesco Dimuro <francesco.dimuro@adecco.it>
2024-02-28 18:58:51 +0100

    feat: new commands integration and refactoring code

--- __main__.py
+++ __main__.py
@@ -5,6 +5,7 @@ from handlers.command.start import start
 from handlers.command.events import events
 from handlers.command.jobs import jobs
 from handlers.command.cmds import cmds
+from handlers.conversation.create_job.handler import create_job_handler
 from handlers.message.unknown import unknown
 import logging
 from telegram.ext import filters
@@ -34,6 +35,24 @@ if __name__ == '__main__':
     
     cmds_handler = CommandHandler('cmds', cmds)
     application.add_handler(cmds_handler)
+
+    # create_job_handler = ConversationHandler(
+    #     entry_points=[CommandHandler("create_job", create_job)],
+    #     states={
+    #         CreateJob.CATEGORY_ID: [MessageHandler(filters.TEXT, callback=category_id)], # spostare tutto in un dict
+    #         CreateJob.CONTRACT_TYPE: [MessageHandler(filters.TEXT, contract_type)],
+    #         CreateJob.POSITION: [
+    #             MessageHandler(filters.TEXT, position),
+    #         ],
+    #         CreateJob.DESCRIPTION: [MessageHandler(filters.TEXT, description)],
+    #         CreateJob.LINK: [MessageHandler(filters.TEXT, link)],
+    #         CreateJob.RAL: [MessageHandler(filters.TEXT, ral)]
+    #     },
+    #     fallbacks=[CommandHandler("cancel", lambda _: ConversationHandler.END)],
+    # )
+
+    create_job_handler = ConversationHandler(**create_job_handler)
+    application.add_handler(create_job_handler)
     
     unknown_handler = MessageHandler(filters.COMMAND, unknown)
     application.add_handler(unknown_handler)
