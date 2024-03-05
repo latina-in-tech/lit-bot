1eccff38d084f9027e44a9c531e6a785cf1d17a6
FrancescoDiMuro <dimurofrancesco@virgilio.it>
2024-03-05 17:42:35 +0100

    feat: add menu button when bot starts

--- handlers/command/start.py
+++ handlers/command/start.py
@@ -1,4 +1,5 @@
-from telegram import Update
+from handlers.command.cmds import COMMANDS_LIST
+from telegram import Update, BotCommand, MenuButton, MenuButtonCommands
 from telegram.ext import ContextTypes
 from telegram.constants import ParseMode
 
@@ -13,5 +14,16 @@ async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
     global START_MESSAGE
     
     START_MESSAGE = START_MESSAGE.format(user_full_name=update.effective_user.full_name)
-    
+
+    # Get the list of available commands
+    bot_commands: list[BotCommand] = [BotCommand(command_name, command_description) 
+                                      for command_name, command_description in COMMANDS_LIST.items()]
+
+    # Set list of commands to the bot
+    await context.bot.set_my_commands(commands=bot_commands)
+
+    # Set menu button to show available bot commands
+    await update.effective_chat.set_menu_button(menu_button=MenuButton(type=MenuButtonCommands.COMMANDS))
+
+    # Send start message
     await context.bot.send_message(chat_id=update.effective_chat.id, text=START_MESSAGE, parse_mode=ParseMode.HTML)
