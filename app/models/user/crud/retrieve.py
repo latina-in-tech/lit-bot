0e21dc6fdb12ebe02ebd061264441405dd178830
FrancescoDiMuro <dimurofrancesco@virgilio.it>
2024-03-16 19:40:57 +0100

    feat: add function to check if a user has the administrator role

--- app/models/user/crud/retrieve.py
+++ app/models/user/crud/retrieve.py
@@ -31,3 +31,17 @@ async def retrieve_user_by_username(username: str) -> User | None:
         query_result: ScalarResult | None = db_session.scalar(sql_statement)
 
         return query_result if query_result else None
+    
+
+async def user_is_administrator(user_telegram_id: int) -> bool | None:
+
+    # Initialize the db_session
+    # It closes automatically at the end of the "with" context manager
+    with SessionLocal() as db_session:
+    
+        sql_statement: Select = select(User) \
+                                .where(User.telegram_id == user_telegram_id)
+        
+        user: User = db_session.scalar(sql_statement)
+        
+        return user.user_role.name == 'Administrator' if user else None
