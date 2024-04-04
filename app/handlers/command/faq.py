c03f300f9f4a6717733952962797cc923de19e84
FrancescoDiMuro <dimurofrancesco@virgilio.it>
2024-04-04 13:28:57 +0200

    feat: add characters and emojis enums

--- app/handlers/command/faq.py
+++ app/handlers/command/faq.py
@@ -1,17 +1,18 @@
 from telegram import Update
 from telegram.ext import ContextTypes
 from telegram.constants import ParseMode
+from utils.constants import Emojis, Characters
 
 
-MESSAGE: str = '''\U00002753 <b>FAQ:</b>
+MESSAGE: str = f'''{Emojis.red_question_mark} <b>FAQ:</b>
 <i>Che cos'è Latina in Tech (LiT)?</i>
-Si tratta di una comunita' (lo so, fa tanto centro di recupero per tossicodipendenti \U0001F62C), 
+Si tratta di una comunita' (lo so, fa tanto centro di recupero per tossicodipendenti {Emojis.grinning_face_with_smiling_eyes}), 
 il cui fulcro e' un gruppo Telegram suddiviso in topics, con l'obiettivo di mettere in contatto appassionati
 del mondo tech nel contesto geografico di Latina e dintorni.
 
 <i>Qual è il suo obiettivo?</i>
-\U00002022 Favorire la condivisione di conoscenza e la crescita professionale nel settore IT;
-\U00002022 Entrare in contatto (sia telematicamente che tramite eventi in presenza) con persone con interessi in comune nell'ambito della tecnologia e affini;
+{Characters.circle} Favorire la condivisione di conoscenza e la crescita professionale nel settore IT;
+{Characters.circle} Entrare in contatto (sia telematicamente che tramite eventi in presenza) con persone con interessi in comune nell'ambito della tecnologia e affini;
 
 <i>Ma quindi, cosa ci si guadagna?</i>
 Niente... o <b>tutto</b>!
@@ -19,7 +20,7 @@ LiT e' una possibilita' interessante di <b>arricchire il proprio background</b> 
 grazie al grande bacino di persone dal quale attingere per trovare <b>collaboratori</b> e per <b>dare visibilita' alle proprie idee</b> di progetto,
 anche solamente per un consiglio di approccio ad una determinata problematica.
 
-<b>TL;DR</b>: conoscere persone con cui sedersi davanti a una birra (pardon, Coca-Cola per gli astemi \U0001F602).
+<b>TL;DR</b>: conoscere persone con cui sedersi davanti a una birra (pardon, Coca-Cola per gli astemi {Emojis.face_with_tears_of_joy}).
 '''
 
 async def faq(update: Update, context: ContextTypes.DEFAULT_TYPE):
