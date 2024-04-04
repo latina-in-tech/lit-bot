45a3ee3cf796516e59c01b0ed78faacf0d31086e
FrancescoDiMuro <dimurofrancesco@virgilio.it>
2024-04-04 13:52:54 +0200

    fix: fix enum names and values

--- app/handlers/command/contacts.py
+++ app/handlers/command/contacts.py
@@ -1,13 +1,13 @@
 from telegram import Update
 from telegram.ext import ContextTypes
 from telegram.constants import ParseMode
-from utils.constants import Emojis
+from utils.constants import Emoji
 
 
 LINKS: dict = {
-    f'{Emojis.up_right_arrow} Telegram': 'https://t.me/+UnoCoBSIPtE1OTNk',
-    f'{Emojis.construction_worker} LinkedIn': 'https://www.linkedin.com/company/latina-in-tech/',
-    f'{Emojis.globe_with_meridians} GitHub': 'https://latina-in-tech.github.io/'
+    f'{Emoji.UP_RIGHT_ARROW} Telegram': 'https://t.me/+UnoCoBSIPtE1OTNk',
+    f'{Emoji.CONSTRUCTION_WORKER} LinkedIn': 'https://www.linkedin.com/company/latina-in-tech/',
+    f'{Emoji.GLOBE_WITH_MERIDIANS} GitHub': 'https://latina-in-tech.github.io/'
 }
 
 
