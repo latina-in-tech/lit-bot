3af6815cda63372e52e188f1acfc62bf7404a086
FrancescoDiMuro <dimurofrancesco@virgilio.it>
2024-04-04 20:40:03 +0200

    chore: code refactoring

--- app/handlers/command/cmds.py
+++ app/handlers/command/cmds.py
@@ -1,93 +1,28 @@
-from telegram import Update, ChatMember
+from telegram import Update
 from telegram.ext import ContextTypes
 from telegram.constants import ParseMode
+from utils.utils import is_user_group_administrator
+from utils.constants import BOT_COMMANDS, Emoji, ChatId
 
-
-# Chat ID of the group chat (General topic)
-GENERAL_CHAT_ID: int = -1001847839591
-
-COMMANDS_LIST: list = [
-    {
-        'name': '/cmds', 
-        'description': 'Visualizza la lista dei comandi eseguibili dal bot', 
-        'requires_admin': False
-    },
-    {
-        'name': '/contacts', 
-        'description': 'Visualizza i contatti della LiT', 
-        'requires_admin': False
-    },
-    {
-        'name': '/create_job', 
-        'description': 'Crea una nuova offerta di lavoro', 
-        'requires_admin': False
-    },
-    {
-        'name': '/events', 
-        'description': 'Visualizza la lista degli eventi della community in programma', 
-        'requires_admin': False
-    },
-    {
-        'name': '/faq', 
-        'description': 'Visualizza le FAQ del gruppo', 
-        'requires_admin': False
-    },
-    {
-        'name': '/get_user_role', 
-        'description': 'Ottiene il ruolo dell\'utente specificato', 
-        'requires_admin': True
-    },
-    {
-        'name': '/jobs', 
-        'description': 'Visualizza la lista dei lavori proposti dai membri della community', 
-        'requires_admin': False
-    },
-    {
-        'name': '/rules', 
-        'description': 'Visualizza le regole del gruppo', 
-        'requires_admin': False
-    },
-    {
-        'name': '/set_user_role', 
-        'description': 'Imposta il ruolo dell\'utente specificato', 
-        'requires_admin': False
-    },
-    {
-        'name': '/slides', 
-        'description': 'Visualizza il link per scaricare i template delle slides per i talk della LiT', 
-        'requires_admin': False
-    },
-    {
-        'name': '/start', 
-        'description': 'Avvia il bot', 
-        'requires_admin': False
-    }
-]
 
 async def cmds(update: Update, context: ContextTypes.DEFAULT_TYPE):
     
-    user_commands: list = []
-    admin_commands: list = []
+    # Message header
+    message: str = f'{Emoji.TECHNOLOGIST} <b>Lista dei comandi disponibili:</b>\n'
     
-    chat_member = await context.bot.get_chat_member(chat_id=GENERAL_CHAT_ID, 
-                                                    user_id=update.effective_user.id)
-
-    # Compose the text to show to the user
-    for command in COMMANDS_LIST:
-        if not command['requires_admin']:
-            user_commands.append(command)
+    # Getting the group Administrator status of the user
+    user_is_group_administrator = is_user_group_administrator(bot=context.bot, 
+                                                              chat_id=ChatId.GENERAL,
+                                                              user_id=update.effective_chat.id)
         
-        admin_commands.append(command)
+    # Getting the bot commands based on the user role in the group
+    commands = (BOT_COMMANDS 
+                if user_is_group_administrator 
+                else filter(lambda command: command['requires_admin'] == False, 
+                            BOT_COMMANDS))
     
-    if chat_member.status == ChatMember.ADMINISTRATOR:
-        message: str = '\U0001F4BB <b>Lista dei comandi disponibili:</b>\n'
-    
-        for command in admin_commands:
-            message += f'{command['name']} - {command['description']}\n'
-    
-    else:
-        
-        for command in user_commands:
-            message += f'{command['name']} - {command['description']}\n'
+    # Compose the list of commands
+    for command in commands:
+        message += f'{command['name']} - {command['description']}\n'
 
     await update.message.reply_text(text=message, parse_mode=ParseMode.HTML)
