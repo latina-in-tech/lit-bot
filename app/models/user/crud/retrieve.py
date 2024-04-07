0d2e109ddfc387e7114927570b636576ca08a818
FrancescoDiMuro <dimurofrancesco@virgilio.it>
2024-04-07 12:56:00 +0200

    feat: add function to retrieve users by their role

--- app/models/user/crud/retrieve.py
+++ app/models/user/crud/retrieve.py
@@ -1,5 +1,6 @@
 from dependencies.db import SessionLocal
 from models.user.user import User
+from models.user_role.crud.retrieve import retrieve_user_role_by_name
 from sqlalchemy import ScalarResult, Select, select
 
 
@@ -48,3 +49,24 @@ async def check_user_role(user_telegram_id: int, user_role: str | list) -> bool 
             return user.user_role.name == user_role if user else None
         elif isinstance(user, list):
             return user.user_role.name in user_role if user else None
+        
+
+async def retrieve_users_by_role(role_name: str) -> list[User] | User:
+    
+    users: list[User] = []
+    
+    # Initialize the db_session
+    # It closes automatically at the end of the "with" context manager
+    with SessionLocal() as db_session:
+
+        user_role = await retrieve_user_role_by_name(user_role_name=role_name)
+
+        sql_statement: Select = select(User) \
+                                .where(User.role_id == user_role.id)
+        
+        query_result: ScalarResult = db_session.scalars(sql_statement)
+
+        if query_result:
+            users = [user for user in query_result]
+
+        return users
