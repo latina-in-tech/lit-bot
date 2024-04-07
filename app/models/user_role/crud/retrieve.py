77e4858283dab74185355d5dadd148b366469bb4
FrancescoDiMuro <dimurofrancesco@virgilio.it>
2024-04-07 18:28:37 +0200

    feat: add function to retrieve user roles

--- app/models/user_role/crud/retrieve.py
+++ app/models/user_role/crud/retrieve.py
@@ -2,6 +2,21 @@ from dependencies.db import SessionLocal
 from models.user_role.user_role import UserRole
 from sqlalchemy import ScalarResult, Select, select
 
+
+async def retrieve_user_roles():
+    
+    # Initialize the db_session
+    # It closes automatically at the end of the "with" context manager
+    with SessionLocal() as db_session:
+    
+        sql_statement: Select = select(UserRole) \
+                                .order_by(UserRole.name)
+            
+        user_roles: ScalarResult | None = [user_role for user_role in db_session.scalars(sql_statement)]
+
+        return user_roles if user_roles else None
+    
+
 async def retrieve_user_role_by_name(user_role_name: str) -> UserRole | None:
     
     # Initialize the db_session
