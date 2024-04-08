d299b65218de1eca5bce9e084df6a388eebac755
FrancescoDiMuro <dimurofrancesco@virgilio.it>
2024-04-08 18:06:56 +0200

    feat: add logic to retrieve group telegram's administrators

--- app/handlers/command/staff.py
+++ app/handlers/command/staff.py
@@ -1,9 +1,7 @@
 from telegram import Update
 from telegram.ext import ContextTypes
 from telegram.constants import ParseMode
-from models.user_role.crud.retrieve import retrieve_user_roles
-from models.user.crud.retrieve import retrieve_users_by_role
-from utils.constants import Character, Emoji
+from utils.constants import Character, ChatId, Emoji
 
 
 async def staff(update: Update, context: ContextTypes.DEFAULT_TYPE):
@@ -13,12 +11,21 @@ async def staff(update: Update, context: ContextTypes.DEFAULT_TYPE):
                     f'{Character.CIRCLE} @andreacoluzzi\n\n'
                     f'Amministratori {Emoji.POLICE_OFFICER}\n')
 
-    # Get the list of administrators
-    if administrators := await retrieve_users_by_role(role_name='Administrator'):
-        
+    # Get the list of the Group Telegram's Administrators
+    # We are accessing the User object of the ChatMemberAdministrator
+    administrators = [
+        chat_member.user.username 
+        for chat_member in await context.bot.get_chat_administrators(chat_id=ChatId.GENERAL)
+        if (chat_member.user.is_bot == False) and (chat_member.user.username is not None)
+    ]
+    
+    # Sort the list (it works only if there are UTF-8 characters as strings)
+    administrators.sort(key=str.lower)
+    
+    if administrators:    
         # Add the administrator to the list of staff
         for administrator in administrators:
-            message += f'{Character.CIRCLE} @{administrator.username}\n'
+            message += f'{Character.CIRCLE} @{administrator}\n'
 
     await update.message.reply_text(text=message, 
                                     parse_mode=ParseMode.HTML)
