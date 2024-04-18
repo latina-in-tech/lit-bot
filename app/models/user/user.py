86e41079cd0c0de67f293b705d7677e702d1098b
FrancescoDiMuro <dimurofrancesco@virgilio.it>
2024-04-18 17:24:44 +0200

    feat: add relationships with related models

--- app/models/user/user.py
+++ app/models/user/user.py
@@ -22,5 +22,7 @@ class User(Base):
     updated_at: Mapped[datetime] = mapped_column(nullable=True)
     deleted_at: Mapped[datetime] = mapped_column(nullable=True)
 
-    user_role = relationship(argument='UserRole', back_populates='users', lazy='joined')
+    events = relationship(argument='Event', back_populates='user')
+    jobs = relationship(argument='Job', back_populates='user')
     memos = relationship(argument='Memo', back_populates='user', lazy='joined')
+    user_role = relationship(argument='UserRole', back_populates='users', lazy='joined')
